45481ce802b0a74c9df0f2225d069ed19ca9470e
quantumastrid <astrid.dymling@consid.se>
2025-10-20 11:52:37 +0200

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Astrid Dymling on 2025-10-20.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
