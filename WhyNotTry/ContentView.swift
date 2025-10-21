da739940a68df75a9b554b3b90f3b6c7e2b95299
quantumastrid <astrid.dymling@consid.se>
2025-10-21 11:50:17 +0200

    Genomförd tutorial i swift, med lite extra effekter

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -1,21 +1,57 @@
-//
-//  ContentView.swift
-//  WhyNotTry
-//
-//  Created by Astrid Dymling on 2025-10-20.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Golf", "Soccer", "Tennis"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    
+    @State private var selected = "Golf"
+    @State private var id = 1
+    @State private var currentColor: Color = .blue // Ny state för färg
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.orange, .red, .purple],                startPoint: .topLeading,
+                           endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+
+            VStack {
+                Text("Why not try...")
+                    .font(.largeTitle.bold())
+                    .padding(.top)
+
+                Spacer()
+
+                VStack {
+                    Circle()
+                        .fill(currentColor)
+                        .shadow(radius: 10)
+                        .padding()
+                        .overlay(
+                            Image(systemName: "figure.\(selected.lowercased())")
+                                .font(.system(size: 144))
+                                .foregroundColor(.white)
+                        )
+                    Text("\(selected)!")
+                        .font(.title)
+                }
+                .transition(.slide)
+                .id(id)
+
+                Spacer()
+
+                Button("Try again") {
+                    withAnimation(.easeInOut(duration: 1)) {
+                        selected = activities.randomElement() ?? "Golf"
+                        currentColor = colors.randomElement() ?? .blue
+                        id += 1
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
